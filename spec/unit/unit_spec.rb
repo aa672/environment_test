6413ca1652c6bb1f421bf39c753a2acb3ac8baaf
adrianaguirre <adrianaguirre@tamu.edu>
2022-09-15 15:51:31 -0500

    Lab Book Collection Complete

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'J.K', price: '22', publishDate: '09/05/2000')
   end
 
   it 'is valid with valid attributes' do
@@ -14,4 +14,20 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+  it 'is not valid without a author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a publishDate' do
+    subject.publishDate = nil
+    expect(subject).not_to be_valid
+  end
+
 end
